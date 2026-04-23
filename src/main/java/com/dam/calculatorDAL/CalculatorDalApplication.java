e7294f36ac34bcc59341c6733e08deab7622a434
Leireda <leireda52@educastur.es>
2026-04-23 20:21:26 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/dam/calculatorDAL/CalculatorDalApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorDAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorDalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorDalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/calculatorDAL/CalculatorDalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorDAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorDalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
